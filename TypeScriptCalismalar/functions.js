975c392ba81d088bc6560cdf801ad06688487820
Abdulkadir Yıldız <yldz788@gmail.com>
2023-08-26 16:07:57 +0300

    functions örnekleri

---
+++ TypeScriptCalismalar/functions.js
@@ -0,0 +1,28 @@
+function topla(x, y) {
+    return x + y;
+}
+//topla2 sadece sayı değerleri alıp sayı değerleri dönebilir.
+function topla2(x, y) {
+    return x + y;
+}
+var topla3 = function (x, y) {
+    return x + y;
+};
+console.log(topla(2, 3));
+console.log(topla("Ankara", 2));
+console.log(topla2(2, 4));
+console.log(topla3(4, 8));
+//ikinci girdiye default değer vermek
+function topla4(x, y) {
+    if (y === void 0) { y = 4; }
+    return x + y;
+}
+console.log(topla4(3));
+//default değer hep en sonda olmalıdır.
+function topla5(x, y) {
+    if (y) {
+        return x + y;
+    }
+    return x;
+}
+console.log(topla5(3, 40));
---
+++ TypeScriptCalismalar/functions.ts
@@ -0,0 +1,33 @@
+function topla(x, y) {
+  return x + y;
+}
+//topla2 sadece sayı değerleri alıp sayı değerleri dönebilir.
+function topla2(x: number, y: number): number {
+  return x + y;
+}
+
+let topla3 = function (x: number, y: number): number {
+  return x + y;
+};
+
+console.log(topla(2, 3));
+console.log(topla("Ankara", 2));
+console.log(topla2(2, 4));
+console.log(topla3(4, 8));
+
+//ikinci girdiye default değer vermek
+function topla4(x: number, y: number = 4): number {
+  return x + y;
+}
+
+console.log(topla4(3));
+
+//default değer hep en sonda olmalıdır.
+function topla5(x: number, y?: number): number {
+  if (y) {
+    return x + y;
+  }
+  return x;
+}
+
+console.log(topla5(3,40));
