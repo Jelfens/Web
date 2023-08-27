07cfb66a7301e02c1ce1613b12954dd851af253c
Abdulkadir Yıldız <yldz788@gmail.com>
2023-08-27 18:22:09 +0300

    interface sonu

--- TypeScriptCalismalar/abstractClasses.ts
+++ TypeScriptCalismalar/abstractClasses.ts
@@ -16,3 +16,25 @@ class TuketiciKredisi extends KrediBase{
         console.log("Tüketici kredisine göre hesaplandı.")
     }
 }
+
+class MortgageKredisi extends KrediBase{
+    constructor(){
+        super();
+    }
+    hesapla(): void {
+        console.log("Mortgage kredisine göre hesaplandı.")
+    }
+}
+
+let tuketiciKredisi = new TuketiciKredisi()
+tuketiciKredisi.hesapla();
+tuketiciKredisi.kaydet();
+
+let mortgageKredisi = new MortgageKredisi()
+mortgageKredisi.hesapla();
+mortgageKredisi.kaydet();
+
+let kredi : KrediBase
+kredi = new TuketiciKredisi()
+
+kredi = new MortgageKredisi()
