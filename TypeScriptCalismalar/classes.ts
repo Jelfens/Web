26cb47cb0f99d69b0bf1b4314c9340efba8c0dfa
Abdulkadir Yıldız <yldz788@gmail.com>
2023-08-27 17:19:49 +0300

    classes sonu

--- TypeScriptCalismalar/classes.ts
+++ TypeScriptCalismalar/classes.ts
@@ -1,17 +1,55 @@
-class Ev{
-    odaSayisi:number;
-    pencereSayisi:number;
-    kat:number;
-    constructor(odaSayisi:number,pencereSayisi:number,kat:number){
-       this.odaSayisi=odaSayisi
-       this.pencereSayisi=pencereSayisi
-       this.kat=kat
+class Ev {
+   odaSayisi: number;
+  pencereSayisi: number;
+  kat: number;
+  constructor(odaSayisi: number, pencereSayisi: number, kat: number) {
+    this.odaSayisi = odaSayisi;
+    this.pencereSayisi = pencereSayisi;
+    this.kat = kat;
+  }
+  yemekYe() {
+    console.log(this.kat + " katlı evde " + "Yemek Yiyildi.");
+  }
+}
+
+let ev = new Ev(3, 4, 5);
+ev.yemekYe();
+console.log(ev.kat);
+
+class Kisi {
+    private isim:string
+
+    get gs_isim():string{
+        return "Sayın " + this.isim
     }
-    yemekYe(){
-        console.log(this.kat +" katlı evde "+"Yemek Yiyildi.")
+    set gs_isim(isim:string){
+        this.isim=isim
+    }
+    kaydet() {
+    console.log("Kişi Kaydedildi.");
+  }
+}
+
+class Musteri extends Kisi{
+    satisYap(){
+        console.log("Satış Yapıldı.")
     }
 }
 
-let ev = new Ev(3,4,5)
-ev.yemekYe()
-console.log(ev.kat)
+class Personel extends Kisi{
+    maasOde(){
+        console.log("Maaş Ödendi.")
+    }
+}
+
+let musteri = new Musteri()
+musteri.gs_isim="Abdulkadir"
+console.log(musteri.gs_isim)
+musteri.kaydet();
+musteri.satisYap();
+
+let personel = new Personel();
+personel.kaydet();
+personel.maasOde()
+
+
