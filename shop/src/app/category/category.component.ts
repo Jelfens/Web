18859e400c49c7aa9f29f6faaaba2cf2f33093fb
Abdulkadir Yıldız <yldz788@gmail.com>
2023-09-01 15:53:16 +0300

    01-09-23

---
+++ shop/src/app/category/category.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Category } from './category';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent {
+title = "Kategori Listesi"
+categories : Category[]= [
+  {id:1, name:"Elektronik"},
+  {id:2, name:"Bilgisayar"}
+]
+}
---
+++ shop/src/app/product/product.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Product } from './product';
+
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+  constructor() {  }
+  title = "Ürün Listesi"
+  products : Product[] = [
+    {id:1, name : "Laptop",price:2500,categoryId:1,description:"Asus Zenbook",imageUrl:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"},
+    {id:2, name : "Mouse",price:25,categoryId:2,description:"A4Tech",imageUrl:"https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1365&q=80"}
+  ] 
+}
